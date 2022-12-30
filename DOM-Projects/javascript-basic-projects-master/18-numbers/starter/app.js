bc05de90f280ca58baddcf59cfe8cd7c0bafe51a
Kavya Sampath Kumar <ksampathkumar@zuora.com>
2022-12-30 19:37:55 +0530

    numbers and dark mode projs added

--- DOM-Projects/javascript-basic-projects-master/18-numbers/starter/app.js
+++ DOM-Projects/javascript-basic-projects-master/18-numbers/starter/app.js
@@ -1,1 +1,24 @@
-console.log('numbers project');
+const items = [...document.querySelectorAll(".number")]; //nodelist -> array
+
+// arrow functions must be defined before calling it, hoisting doesnt work with arrow functions
+const updateCount = (el) => {
+    const value = parseInt(el.dataset.value);
+    const increment = Math.ceil(value / 1000);
+    let initialValue = 0;
+ 
+    const increaseCount = setInterval( () => {
+        initialValue += increment;
+
+        if(initialValue > value) {
+            el.textContent = `${value}+`;
+            clearInterval(increaseCount);
+            return;
+        }
+        el.textContent = `${initialValue}+`
+    }, 1);
+}
+
+items.forEach( (item) => {
+    updateCount(item);
+});
+
