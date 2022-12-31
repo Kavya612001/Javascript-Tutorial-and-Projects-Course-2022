70a1f364c15fa8a31950f9eb711f8714541ee6b4
Kavya Sampath Kumar <ksampathkumar@zuora.com>
2022-12-31 11:41:29 +0530

    dad joke proj addded

--- DOM-Projects/javascript-basic-projects-master/21-dad-jokes/starter/app.js
+++ DOM-Projects/javascript-basic-projects-master/21-dad-jokes/starter/app.js
@@ -1,1 +1,32 @@
-console.log('Dad Jokes Starter');
+const url = 'https://icanhazdadjoke.com/scss';
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", ()=>{
+    fetchDadJoke();
+})
+
+const fetchDadJoke = async ()=> {
+    result.textContent = 'Loading...';
+
+    try {
+        const response = await fetch(url, {
+            //info abt the request
+            headers: {
+                Accept: 'application/json',
+                'User-Agent': 'learning app'
+            }
+        })
+        // if the fetch url is wrong 
+        if(!response.ok) {
+            throw new Error('there was an error');
+        }
+        const data = await response.json();
+        result.textContent = data.joke;
+    } catch (error) {
+        result.textContent = 'There was an error...';
+    }
+}
+
+fetchDadJoke();
