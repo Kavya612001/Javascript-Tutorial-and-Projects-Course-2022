a3ef44dd699f36fa792c3f8ce771f9b2bc06fd40
Kavya Sampath Kumar <ksampathkumar@zuora.com>
2023-01-02 00:11:40 +0530

    cocktails api proj added

---
+++ DOM-Projects/javascript-basic-projects-master/24-cocktails/setup/singleDrink.js
@@ -0,0 +1,18 @@
+import fetchDrinks from "./src/fetchDrinks.js";
+import displayDrink from "./src/displaySingleDrink.js";
+
+
+const presentDrink = async () => {
+
+    const id = localStorage.getItem('drink');
+    if(!id) {
+        // if there is no id in the local storage, it redirects to index.html
+        window.location.replace("index.html");
+    }
+    else {
+        const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+        displayDrink(drink);
+    }
+}
+
+window.addEventListener("DOMContentLoaded", presentDrink);
---
+++ DOM-Projects/javascript-basic-projects-master/24-cocktails/setup/src/searchForm.js
@@ -0,0 +1,14 @@
+import get from "./getElement.js";
+import presentDrinks from "./presentDrinks.js";
+
+const baseUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+const form = get(".search-form");
+const input = get('[name="drink"]');
+
+form.addEventListener("keyup", function (e) {
+    e.preventDefault();
+    const value = input.value;
+    if(!value) return;
+    presentDrinks(`${baseUrl}${value}`);
+});
